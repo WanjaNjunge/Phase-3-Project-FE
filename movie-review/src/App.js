391e521ed580b003c26e06251968d82e9ed0ff6e
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-06-09 11:30:14 +0300

    added review page

--- movie-review/src/App.js
+++ movie-review/src/App.js
@@ -1,58 +1,86 @@
-import React, { useState, useEffect } from "react";
-import LoginForm from "./components/LoginForm";
-import MovieList from "./components/MovieList";
-import ReviewForm from "./components/ReviewForm";
-import { login, getMovies, createReview } from "./api";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import MovieDetails from './components/MovieDetails';
+import './App.css';
 
-const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+const API_KEY = '79b6e476480f1e9ea75e81bc7be0cdf6';
+
+function App() {
   const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
-    if (loggedIn) {
-      // Fetch movies when the user is logged in
-      getMovies()
-        .then((data) => setMovies(data))
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+
+    if (searchQuery.trim() !== '') {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchQuery}`
+      )
+        .then((response) => response.json())
+        .then((data) => setSearchResults(data.results))
         .catch((error) => console.log(error));
     } else {
-      // Clear movies when the user logs out
-      setMovies([]);
+      setSearchResults([]);
     }
-  }, [loggedIn]);
-
-  const handleLogin = (credentials) => {
-    login(credentials)
-      .then(() => setLoggedIn(true))
-      .catch((error) => console.log(error));
-  };
-
-  const handleLogout = () => {
-    setLoggedIn(false);
-  };
-
-  const handleCreateReview = (reviewData) => {
-    createReview(reviewData)
-      .then((data) => {
-        // Add the created review to the list
-        setMovies([...movies, data]);
-      })
-      .catch((error) => console.log(error));
   };
 
   return (
-    <div>
-      {!loggedIn ? (
-        <LoginForm onLogin={handleLogin} />
-      ) : (
-        <>
-          <h2>Movie List</h2>
-          <MovieList movies={movies} />
-          <ReviewForm onCreateReview={handleCreateReview} />
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      )}
+    <BrowserRouter>
+      <div>
+        <h1>Review Movies</h1>
+
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+            placeholder="Search for a movie..."
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        <Routes>
+          <Route path="/" element={<Home movies={movies} searchResults={searchResults} />} />
+          <Route path="/movie/:movieId" element={<MovieDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Home({ movies, searchResults }) {
+  const BASE_URL = 'https://image.tmdb.org/t/p/w500';
+  const displayedMovies = searchResults.length > 0 ? searchResults : movies;
+
+  return (
+    <div className="popular-movies">
+      <h2>Popular Movies</h2>
+      <div className="movie-grid">
+        {displayedMovies.map((movie) => (
+          <div key={movie.id} className="movie-item">
+            <h2>
+              <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+            </h2>
+            {movie.poster_path && (
+              <img src={`${BASE_URL}${movie.poster_path}`} alt={movie.title} />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default App;
