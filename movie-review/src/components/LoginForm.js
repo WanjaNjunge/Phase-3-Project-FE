7f3898f27919316f7113b973139acba7c4c58849
Noah <noahmwangi@gmail.com>
2023-06-09 06:53:14 +0300

    some changes to login

--- movie-review/src/components/LoginForm.js
+++ movie-review/src/components/LoginForm.js
@@ -1,80 +1,86 @@
-import React, { useState } from "react";
-import { login, signUp } from "../api";
+import React, { useState } from 'react';
 
-const LoginForm = ({ onLogin }) => {
-  const [formType, setFormType] = useState("login");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState(""); // Add this line
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (formType === "login") {
-      login(name, email, password)
-        .then((user) => {
-          // Handle successful login
-          onLogin(user);
-        })
-        .catch((error) => {
-          // Handle login error
-          console.error("Login error:", error);
-        });
-    } else if (formType === "signup") {
-      signUp(name, email, password)
-        .then((user) => {
-          // Handle successful sign-up
-          onLogin(user);
-        })
-        .catch((error) => {
-          // Handle sign-up error
-          console.error("Sign-up error:", error);
-        });
+  const handleLogin = async () => {
+    try {
+      // Send a POST request to the login endpoint with user credentials
+      const response = await fetch('http://localhost:9292/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      console.log('Login response data:', data); // Log the response data
+
+      if (response.ok) {
+        // Login successful, perform necessary actions (e.g., store authentication token)
+        console.log('Login successful');
+        setMessage('Login successful');
+      } else {
+        // Login failed, display error message
+        console.log('Login failed:', data.message);
+        setMessage('Login failed');
+      }
+    } catch (error) {
+      console.log('Error:', error.message);
+      setMessage('An error occurred');
+    }
+  };
+
+  const handleRegister = async () => {
+    try {
+      // Send a POST request to the register endpoint with user credentials
+      const response = await fetch('http://localhost:9292/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      console.log('Register response data:', data); // Log the response data
+
+      if (response.ok) {
+        // Registration successful, perform necessary actions
+        console.log('Registration successful');
+        setMessage('Registration successful');
+      } else {
+        // Registration failed, display error message
+        console.log('Registration failed:', data.message);
+        setMessage('Registration failed');
+      }
+    } catch (error) {
+      console.log('Error:', error.message);
+      setMessage('An error occurred');
     }
   };
 
   return (
     <div>
-      <h2>{formType === "login" ? "Login" : "Sign Up"}</h2>
-      <form onSubmit={handleSubmit}>
-        {/* Form fields */}
-        <input
-          type="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          required
-        />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        {/* Additional form fields for sign-up */}
-        {formType === "signup" && (
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder="Confirm Password"
-            required
-          />
-        )}
-        {/* Form buttons */}
-        <button type="submit">{formType === "login" ? "Login" : "Sign Up"}</button>
-        <button type="button" onClick={() => setFormType(formType === "login" ? "signup" : "login")}>
-          {formType === "login" ? "Switch to Sign Up" : "Switch to Login"}
-        </button>
-      </form>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleRegister}>Register</button>
+      <p>{message}</p>
     </div>
   );
 };
